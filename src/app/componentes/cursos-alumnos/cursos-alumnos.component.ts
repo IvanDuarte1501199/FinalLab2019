61043f3c81528e33e1339401353731aec8301515
IvanDuarte1501199 <53486708+IvanDuarte1501199@users.noreply.github.com>
2019-12-04 22:23:29 -0300

    1.9 - se ve el profe

--- src/app/componentes/cursos-alumnos/cursos-alumnos.component.ts
+++ src/app/componentes/cursos-alumnos/cursos-alumnos.component.ts
@@ -4,6 +4,8 @@ import { curso } from 'src/app/modelo/curso';
 import { curso_alumno } from 'src/app/modelo/curso_alumno';
 import { CursoAlumnoRepoService } from 'src/app/servicios/curso-alumno-repo.service';
 import { alumno } from 'src/app/modelo/alumno';
+import { ProfesorRepoService } from 'src/app/servicios/profesor-repo.service';
+import { profesor } from 'src/app/modelo/profesor';
 
 @Component({
   selector: 'app-cursos-alumnos',
@@ -13,21 +15,30 @@ import { alumno } from 'src/app/modelo/alumno';
 export class CursosAlumnosComponent implements OnInit {
 
   listadoCursos: curso[] = [];
-  
+
   nuevoAlumnoDeCurso: curso_alumno = new curso_alumno(null, null);
   cursoElegido: curso = new curso('', null, null, null, null);
-  alumnoDelCurso: alumno = new alumno(null,'','',null);
+  profesorDelCurso: profesor = new profesor(null,'','',null);
+  alumnoDelCurso: alumno = new alumno(null, '', '', null);
 
-  constructor(private _cursoRepoService: CursosRepoService, private _cursoAlumnoRepoService: CursoAlumnoRepoService) {
+  constructor(private _cursoRepoService: CursosRepoService, private _cursoAlumnoRepoService: CursoAlumnoRepoService,
+    private _profesorRepoService: ProfesorRepoService) {
     this.listadoCursos = this._cursoRepoService.devolverCursos();
   }
 
   ngOnInit() {
   }
 
+  verProfesor() {
+    //tira error el primer profe
+    console.log("entro al metodo");
+    this._profesorRepoService.getProfesorById(this.cursoElegido.profesorId).subscribe(
+      data => { this.profesorDelCurso = data }
+    );
+  }
+
   cambiarCurso() {
     console.log("entro al metodo");
-    console.log(this.nuevoAlumnoDeCurso.cursoId);
     this._cursoRepoService.getCursoById(this.nuevoAlumnoDeCurso.cursoId).subscribe(
       data => { this.cursoElegido = data }
     );
